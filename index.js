29fbdec3e4aa3ccfb1ac4d5935eca56843cadc8f
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-01-16 23:41:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ let cor = 0;
 
 $("#title-button").click(function () {
   $("#title-screen").toggle("puff");
+  $("#ques1").show();
   
   document.getElementById("hell").play();
+});
+
+$("#ques1-agree").click(function () {
+  cor = cor + 5;
+  
+  $("#ques1").toggle("puff");
+  $("#ques2").show();
+});
+
+$("#ques1-unsure").click(function () {
+  gd = gd + 2.5;
+  cor = cor + 2.5;
+  
+  $("#ques1").toggle("puff");
+  $("#ques2").show();
+});
+
+$("#ques1-disagree").click(function () {
+  gd = gd + 5;
+  
+  $("#ques1").toggle("puff");
+  $("#ques2").show();
 });
